5e2fa5c869cc4da8460c0a032327cdbf5aae62e2
Benjamin Eckel <bhelx@simst.im>
2016-03-31 23:32:58 -0700

    different zoom level

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -41,7 +41,7 @@ ready(() => {
   var map = L.map('map');
   window.map = map;
 
-  map.setView([window.lastKnownLocation.lat, window.lastKnownLocation.lon], 14);
+  map.setView([window.lastKnownLocation.lat, window.lastKnownLocation.lon], 11);
 
   L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
       attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
